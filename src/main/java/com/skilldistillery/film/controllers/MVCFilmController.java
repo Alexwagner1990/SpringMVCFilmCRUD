a5133378156939eed8448047bc69a5eb0752c9fe
Dora Harper <doraeharper@gmail.com>
2018-04-14 11:11:23 -0600

    Controller

--- src/main/java/com/skilldistillery/film/controllers/MVCFilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/MVCFilmController.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.film.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,30 +12,54 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.skilldistillery.film.dao.DatabaseAccessorInterface;
 import com.skilldistillery.film.entities.Film;
 
-
 @Controller
 public class MVCFilmController {
-  @Autowired
-  private DatabaseAccessorInterface dao;
+	@Autowired
+	private DatabaseAccessorInterface dao;
 
-  public void setStateDAO(DatabaseAccessorInterface dao) {
-    this.dao = dao;
-  }
+	public void setStateDAO(DatabaseAccessorInterface dao) {
+		this.dao = dao;
+	}
 
-  @RequestMapping(path = "GetStateData.do", params = "name", method = RequestMethod.GET)
-  public ModelAndView getStateByName(String name) {
-    ModelAndView mv = new ModelAndView();
-    Film f = dao.getStateByName(name);
-    mv.addObject("state", s);
-    mv.setViewName("WEB-INF/result.jsp");
-    return mv;
-  }
+	@RequestMapping(path = "AddFilm.do", params = "name", method = RequestMethod.GET)
+	public ModelAndView getStateByName(Film film) {
+		ModelAndView mv = new ModelAndView();
+		Film f = dao.addFilm(film);
+		mv.addObject("film", f);
+		mv.setViewName("WEB-INF/result.jsp");
+		return mv;
+	}
 
-  @RequestMapping(path = "GetStateData.do", params = "abbr", method = RequestMethod.GET)
-  public ModelAndView getStateByAbbreviation(String abbr) {
-    ModelAndView mv = new ModelAndView();
-    Film f = dao.getStateByAbbreviation(abbr);
-    mv.addObject("state", s);
-    mv.setViewName("WEB-INF/result.jsp");
-    return mv;
-  }
+	@RequestMapping(path = "DeleteFilm.do", params = "delete", method = RequestMethod.GET)
+	public ModelAndView getStateByAbbreviation(int film) {
+		ModelAndView mv = new ModelAndView();
+		Film f = dao.deleteFilm(film);
+		mv.addObject("film", f);		
+		mv.setViewName("WEB-INF/result.jsp");
+		return mv;
+	}
+	@RequestMapping(path = "FilmById.do", params = "FilmById", method = RequestMethod.GET)
+	public ModelAndView getFilmById(int filmId) {
+		ModelAndView mv = new ModelAndView();
+		Film f = dao.getFilmById(filmId);
+		mv.addObject("film", f);		
+		mv.setViewName("WEB-INF/result.jsp");
+		return mv;
+	}
+	@RequestMapping(path = "UpdateFilm.do", params = "FilmById", method = RequestMethod.GET)
+	public ModelAndView updateFilm(int filmId) {
+		ModelAndView mv = new ModelAndView();
+		Film f = dao.getFilmById(filmId);
+		mv.addObject("film", f);		
+		mv.setViewName("WEB-INF/result.jsp");
+		return mv;
+	}
+	@RequestMapping(path = "FindByKeyword.do", params = "name", method = RequestMethod.GET)
+	public ModelAndView updateFilm(String search) {
+		ModelAndView mv = new ModelAndView();
+		List<Film> f = dao.getFilmBySearchTerm(search);
+		mv.addObject("film", f);		
+		mv.setViewName("WEB-INF/result.jsp");
+		return mv;
+	}
+}
